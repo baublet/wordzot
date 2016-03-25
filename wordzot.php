2ad7bcd49c30266d4f891a5223448c87caa4360b
Ryan Poe <baublet@gmail.com>
2016-03-25 11:25:38 -0400

    More initial commits

---
+++ wordzot.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:    WordZot
+Plugin URI:     http://www.ryanmpoe.com
+Description:    A power Zotero manager for WordPress
+Version:        0.0
+Author:         Ryan Poe
+Author URI:     http://www.ryanmpoe.com
+*/
+
+
+define('_WORDZOT_DEBUG', true);
+require_once("twig-setup.php");
